380818a75c5db116676d7de06f232499bbd12865
shruti3105 <82582531+shruti3105@users.noreply.github.com>
2021-05-05 14:48:52 +0530

    Update FindMaxTest.java

--- lib/src/test/java/FindMax/FindMaxTest.java
+++ lib/src/test/java/FindMax/FindMaxTest.java
@@ -23,26 +23,6 @@ public class FindMaxTest {
         FindMax findMaximum = new FindMax();
         Integer result=findMaximum.maximum(1,2,3);
         Assert.assertSame(3,result);
-    }
-    
-    @Test
-    public void givenMaximumFloatAtPosition1_ShouldReturnFloatOfPosition1() {
-        FindMax findMaximum = new FindMax();
-        Double result = findMaximum.maximum(3.3, 2.2, 3.3);
-        Assert.assertSame(3.3, result);
-    }
-
-    @Test
-    public void givenMaximumFloatAtPosition2_ShouldReturnFloatOfPosition2() {
-        FindMax findMaximum = new FindMax();
-        Double result = findMaximum.maximum(1.1, 3.3, 2.2);
-        Assert.assertSame(3.3, result);
-    }
-
-    @Test
-    public void givenMaximumFloatAtPosition3_ShouldReturnFloatOfPosition3() {
-        FindMax findMaximum = new FindMax();
-        Double result = findMaximum.maximum(1.1, 2.2, 3.3);
-        Assert.assertSame(3.3, result);
+   
     }
 }
