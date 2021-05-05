54e1268bb98d7183d850e9538b80de9fbe075307
shruti3105 <82582531+shruti3105@users.noreply.github.com>
2021-05-05 14:48:13 +0530

    Update FindMaxTest.java

--- lib/src/test/java/FindMax/FindMaxTest.java
+++ lib/src/test/java/FindMax/FindMaxTest.java
@@ -1,30 +1,3 @@
-package FindMax;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-public class FindMaxTest {
-	@Test
-    public void givenMaximumIntegerAtPosition1_ShouldReturnIntegerOfPosition1() {
-        FindMax findMaximum = new FindMax();
-        Integer result=findMaximum.maximum(3,2,1);
-        Assert.assertSame(3,result);
-    }
-
-    @Test
-    public void givenMaximumIntegerAtPosition2_ShouldReturnIntegerOfPosition2() {
-        FindMax findMaximum = new FindMax();
-        Integer result=findMaximum.maximum(1,3,2);
-        Assert.assertSame(3,result);
-    }
-
-    @Test
-    public void givenMaximumIntegerAtPosition3_ShouldReturnIntegerOfPosition3() {
-        FindMax findMaximum = new FindMax();
-        Integer result=findMaximum.maximum(1,2,3);
-        Assert.assertSame(3,result);
-    }
-    
     package FindMax;
 
     import org.junit.Assert;
