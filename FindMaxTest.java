a348ac75a5dc93c77da172718eb5c47994977918
Shruti Verma <shrutiverma3105@gmail.com>
2021-05-05 14:41:03 +0530

    commit test UC1

--- FindMaxTest.java
+++ FindMaxTest.java
@@ -24,5 +24,55 @@ public class FindMaxTest {
         Integer result=findMaximum.maximum(1,2,3);
         Assert.assertSame(3,result);
     }
+    
+    package FindMax;
+
+    import org.junit.Assert;
+    import org.junit.Test;
+
+    public class FindMaxTest {
+    	@Test
+        public void givenMaximumIntegerAtPosition1_ShouldReturnIntegerOfPosition1() {
+            FindMax findMaximum = new FindMax();
+            Integer result=findMaximum.maximum(3,2,1);
+            Assert.assertSame(3,result);
+        }
+
+        @Test
+        public void givenMaximumIntegerAtPosition2_ShouldReturnIntegerOfPosition2() {
+            FindMax findMaximum = new FindMax();
+            Integer result=findMaximum.maximum(1,3,2);
+            Assert.assertSame(3,result);
+        }
+
+        @Test
+        public void givenMaximumIntegerAtPosition3_ShouldReturnIntegerOfPosition3() {
+            FindMax findMaximum = new FindMax();
+            Integer result=findMaximum.maximum(1,2,3);
+            Assert.assertSame(3,result);
+        }
+        
+        @Test
+        public void givenMaximumFloatAtPosition1_ShouldReturnFloatOfPosition1() {
+            FindMax findMaximum = new FindMax();
+            Double result = findMaximum.maximum(3.3, 2.2, 3.3);
+            Assert.assertSame(3.3, result);
+        }
+
+        @Test
+        public void givenMaximumFloatAtPosition2_ShouldReturnFloatOfPosition2() {
+            FindMax findMaximum = new FindMax();
+            Double result = findMaximum.maximum(1.1, 3.3, 2.2);
+            Assert.assertSame(3.3, result);
+        }
+
+        @Test
+        public void givenMaximumFloatAtPosition3_ShouldReturnFloatOfPosition3() {
+            FindMax findMaximum = new FindMax();
+            Double result = findMaximum.maximum(1.1, 2.2, 3.3);
+            Assert.assertSame(3.3, result);
+        }
+    }
+
 }
 
