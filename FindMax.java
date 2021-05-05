6e2fd00d636430c41b35a57e1d55ff4393722f14
Shruti Verma <shrutiverma3105@gmail.com>
2021-05-05 13:52:36 +0530

    commit UC2

--- FindMax.java
+++ FindMax.java
@@ -14,6 +14,15 @@ public class FindMax {
         }
         return max;
     }
-
+public Double maximum(Double num1, Double num2, Double num3) {
+        Double max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        return max;
+    }
 }
 
